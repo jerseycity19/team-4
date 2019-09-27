63795bc3f34ef02fb85ae7a45e05a6908cce0b88
Charles Bethin <cbethin@stevens.edu>
2019-09-27 19:55:19 -0400

    removed node modules -- oops :)

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/api/newaccesscode', (req, res) => {
+
+})
+
+app.post('/api/submitform', (req, res) => {
+    
+})
+
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
