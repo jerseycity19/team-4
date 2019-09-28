ac8060ede0e06ade16ac2b3f56877725907e6e00
Charles Bethin <cbethin@stevens.edu>
2019-09-27 22:38:34 -0400

    Added form submit

--- src/app.js
+++ src/app.js
@@ -16,19 +16,15 @@ const app = express();
 const port = 3000;
 
 // pool.getConnection()
-// .then(conn => console.log("Connection:", conn))
-// .catch(err => console.log("err:", err))
-
-pool.getConnection()
-.then(conn => {
-    // console.log('Connection:', conn);
-    conn.query(`INSERT INTO accesscode value(12, 12, 12, ${(new Date()).getTime()}, ${(new Date()).getTime()}, 1)`)
-    .then(completion => {
-        console.log('Success:', completion);
-    })
-    .catch(err => console.log(err));
-})
-.catch(err => console.log(err));
+// .then(conn => {
+//     // console.log('Connection:', conn);
+//     conn.query(`INSERT INTO accesscode value(12, 12, 12, ${(moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'))}, ${moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')}, 1)`)
+//     .then(completion => {
+//         console.log('Success:', completion);
+//     })
+//     .catch(err => console.log(err));
+// })
+// .catch(err => console.log(err));
 
 function createCode() {
     
@@ -42,8 +38,16 @@ app.get('/api/newaccesscode', (req, res) => {
 
 })
 
-app.post('/api/submitform', (req, res) => {
+app.post('/api/checkAccessCodeValidity', (req, res) => {
+    var isValid = checkValidity(req.params.accessCode);
+    res.json({
+        success: true,
+        isValid: isValid
+    })
+});
 
+app.post('/api/submitform', (req, res) => {
+    
 })
 
 app.use(express.static('FrontEnd'))
