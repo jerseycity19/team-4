e190b957a99dcd7b1a36660d06299914ad23f009
Nasir Kasumu <naskas1234@gmail.com>
2019-09-28 09:29:01 -0400

    relabled charts and updated html in analytics

--- FrontEnd/chart/chart.js
+++ FrontEnd/chart/chart.js
@@ -2,7 +2,7 @@ var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["2015-01", "2015-02", "2015-03", "2015-04", "2015-05", "2015-06", "2015-07", "2015-08", "2015-09", "2015-10", "2015-11", "2015-12"],
+    labels: ["North America","South","Central America","Middle East", "North Africa","Sub-Saharan Africa","East Asia","SE Asia","Europe","Central Asia","Pacific","Turkey"],
     datasets: [{
       label: '# Frequency of Unvalidated Reports',
       data: [12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1],
