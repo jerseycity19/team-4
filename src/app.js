6a7945d06499016f7a307e4698dc051acfc5c45c
Charles Bethin <cbethin@stevens.edu>
2019-09-27 21:49:25 -0400

    Added database connection

--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
 const express = require('express');
+const maraidb = require('mariadb');
+const db_login = require('./database_login.json');
+
+var pool = maraidb.createPool({
+    host: db_login.host, 
+    user: db_login.user, 
+    password: db_login.password,
+    database: db_login.database,
+    connectionLimit: 10
+});
 
 const app = express();
 const port = 3000;
+
+pool.getConnection()
+.then(conn => console.log("Connection:", conn))
+.catch(err => console.log("err:", err))
+
+function createCode() {
+    
+}
+
+function checkValidity(code) {
+
+}
 
 app.get('/api/newaccesscode', (req, res) => {
 
 })
 
 app.post('/api/submitform', (req, res) => {
-    
+
 })
 
-app.use(express.static('public'))
+app.use(express.static('FrontEnd'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
